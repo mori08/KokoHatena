a71aabd2ee9c12a787c5cb4db28f55f09c27f557
mori08 <mrmt0814@gmail.com>
2022-04-23 14:31:12 +0900

    fix: 画面サイズの変更

--- KokoHatena/Src/Main.cpp
+++ KokoHatena/Src/Main.cpp
@@ -1,8 +1,12 @@
 ﻿
-# include<Siv3D.hpp>
+#include"Config/Config.hpp"
 
 void Main()
 {
+	// ウィンドウの設定
+	Window::Resize(Kokoha::Config::get<Size>(U"Window.size"));
+	Window::SetTitle(Kokoha::Config::get<String>(U"Window.name"));
+
 	while (System::Update())
 	{
 	}
