eeff087475b751c5c240944f0fb2747fde9a0367
mori08 <mrmt0814@gmail.com>
2022-07-28 00:30:17 +0900

    #7 feat Boardを末尾へ移動させる関数の実装

--- KokoHatena/Src/ShareData/BoardManager/BoardManager.cpp
+++ KokoHatena/Src/ShareData/BoardManager/BoardManager.cpp
@@ -36,19 +36,19 @@ namespace Kokoha
 		}
 	}
 
+	std::list<std::unique_ptr<Board>>::iterator BoardManager::findBoardItr(const Board::Role& role)
+	{
+		for (auto itr = m_boardList.begin(); itr != m_boardList.end(); ++itr)
+		{
+			if ((*itr)->role() == role) { return itr; }
+		}
+		return m_boardList.end();
+	}
+
 	void BoardManager::displayBoard(const Board::Role& role, const String& requestText)
 	{
 		// 先頭に移動させるイテレータ
-		auto boardItr = m_boardList.end();
-
-		// 指定したロールのボードのイテレータの探索
-		for (auto itr = m_boardList.begin(); itr != m_boardList.end(); ++itr)
-		{
-			if ((*itr)->role() == role)
-			{
-				boardItr = itr; break;
-			}
-		}
+		auto boardItr = findBoardItr(role);
 
 		// ボードが見つからないときエラー
 		if (boardItr == m_boardList.end())
@@ -63,5 +63,25 @@ namespace Kokoha
 
 		// 先頭のボードの状態をIS_DISPLAYEDに変更
 		m_boardList.front()->display();
+	}
+
+	void BoardManager::hideBoard(const Board::Role& role)
+	{
+		// 末尾に移動させるイテレータ
+		auto boardItr = findBoardItr(role);
+
+		// ボードが見つからないときエラー
+		if (boardItr == m_boardList.end())
+		{
+			throw Error(U"Faild to find Board");
+		}
+
+		// 最前面に移動
+		auto boardPtr = std::move(*boardItr);
+		m_boardList.erase(boardItr);
+		m_boardList.emplace_back(std::move(boardPtr));
+
+		// 末尾のボードの状態をIS_HIDINGに変更
+		m_boardList.front()->hide();
 	}
 }
