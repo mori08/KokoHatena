2ffa3574ccaa79e4cf60cb6e6c851fa4fe2ac917
mori08 <mrmt0814@gmail.com>
2022-08-20 15:19:25 +0900

    #16 feat SelectRecordSceneの追加

---
+++ KokoHatena/Src/Scene/SelectRecordScene/SelectRecordScene.cpp
@@ -0,0 +1,19 @@
+#include "SelectRecordScene.hpp"
+
+namespace Kokoha
+{
+	SelectRecordScene::SelectRecordScene(const InitData& init)
+		: IScene(init)
+	{
+	}
+
+	void SelectRecordScene::update()
+	{
+
+	}
+
+	void SelectRecordScene::draw() const
+	{
+
+	}
+}
---
+++ KokoHatena/Src/Scene/SelectRecordScene/SelectRecordScene.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "../Scene.hpp"
+
+namespace Kokoha
+{
+	/// <summary>
+	/// RecordSetを選択するシーン
+	/// 選択したRecordSetをどうするかは派生クラスで決定する
+	/// </summary>
+	class SelectRecordScene : MyApp::Scene
+	{
+	private:
+
+	public:
+
+		SelectRecordScene(const InitData& init);
+
+		void update() override;
+
+		void draw() const override;
+
+	};
+}
