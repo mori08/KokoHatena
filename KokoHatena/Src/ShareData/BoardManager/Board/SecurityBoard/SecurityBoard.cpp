4ad5dec75cb60657b8ca602ea8ea0feedbb396e5
mori08 <mrmt0814@gmail.com>
2023-10-14 23:45:02 +0900

    feat: #70: SecurityBoardクラスの枠組み作成

---
+++ KokoHatena/Src/ShareData/BoardManager/Board/SecurityBoard/SecurityBoard.cpp
@@ -0,0 +1,27 @@
+﻿#include "SecurityBoard.hpp"
+
+namespace Kokoha
+{
+	SecurityBoard::SecurityBoard(const RecordSet& recordSet)
+		: Board(BoardRole::SECURITY
+			, U"SerucityBoard"
+			, BoardState::IS_HIDING)
+	{
+	}
+
+	void SecurityBoard::receiveRequest(const String& requestText)
+	{
+	}
+
+	void SecurityBoard::inputInBoard()
+	{
+	}
+
+	void SecurityBoard::updateInBoard(BoardRequest& request)
+	{
+	}
+
+	void SecurityBoard::drawInBoard() const
+	{
+	}
+}
---
+++ KokoHatena/Src/ShareData/BoardManager/Board/SecurityBoard/SecurityBoard.hpp
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "../Board.hpp"
+#include "../../../RecordSet/RecordSet.hpp"
+
+namespace Kokoha
+{
+	class SecurityBoard : public Board
+	{
+	private:
+
+	public:
+
+		SecurityBoard(const RecordSet& recordSet);
+
+	private:
+
+		void receiveRequest(const String& requestText) override;
+
+		void inputInBoard() override;
+
+		void updateInBoard(BoardRequest& request) override;
+
+		void drawInBoard() const override;
+
+	};
+}
